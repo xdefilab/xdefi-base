e8a104c42b2099835a51d194a25f00992a1cafc1
jingleizhang <eric@xdefi.com>
2021-03-20 00:53:54 +0800

    rm LOG_SINGLE_JOIN

--- contracts/XPool.sol
+++ contracts/XPool.sol
@@ -47,12 +47,6 @@ contract XPool is XApollo, XPToken, XConst {
         uint256 tokenAmountIn
     );
 
-    event LOG_SINGLE_JOIN(
-        address indexed caller,
-        address indexed tokenIn,
-        uint256 tokenAmountIn
-    );
-
     event LOG_EXIT(
         address indexed caller,
         address indexed tokenOut,
@@ -654,7 +648,7 @@ contract XPool is XApollo, XPToken, XConst {
         inRecord.balance = (inRecord.balance).badd(tokenAmountIn);
 
         _pushUnderlying(tokenIn, SAFU, _safuFee);
-        emit LOG_SINGLE_JOIN(msg.sender, tokenIn, tokenAmountIn);
+        emit LOG_JOIN(msg.sender, tokenIn, tokenAmountIn);
         _mintPoolShare(poolAmountOut);
         _pushPoolShare(msg.sender, poolAmountOut);
         return poolAmountOut;
